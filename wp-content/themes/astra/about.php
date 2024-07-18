0e01332625f320e423e671444bf95d73bc86f4ae
Anjali <anjalichauhan193@gmail.com>
2024-07-18 10:33:24 +0530

    Decor project

---
+++ wp-content/themes/astra/about.php
@@ -0,0 +1,34 @@
+<?php
+// Template Name:about
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+get_header(); 
+?>
+
+<?php if ( astra_page_layout() == 'left-sidebar' ) : ?>
+
+	<?php get_sidebar(); ?>
+
+<?php endif ?>
+	<div id="primary" <?php astra_primary_class(); ?>>
+		<?php
+		astra_primary_content_top();
+
+		astra_content_loop();
+
+		astra_pagination();
+
+		astra_primary_content_bottom();
+		?>
+	</div><!-- #primary -->
+    hello
+<?php
+if ( astra_page_layout() == 'right-sidebar' ) :
+
+	get_sidebar();
+
+endif;
+
+get_footer();
