908a3f624b22dcd4d344b2d1eda8a3fe7d33b29b
Anjali <anjalichauhan193@gmail.com>
2024-07-26 10:22:18 +0530

    If session set once it will persist till browser close

--- session-check.php
+++ session-check.php
@@ -4,9 +4,11 @@
  *
  * @package WordPress
  */
-
+session_start();
 if(isset($_GET["access"]) && $_GET["access"]=="80284937-9e16-433b-92c4-9bf873094019"){
 	$_SESSION["access"] = $_GET['access'];	
+ }else if(isset($_SESSION["access"]) && $_SESSION["access"]!=''){
+	$_SESSION["access"] = $_SESSION["access"];
  }else{	
 	 exit;
  }
